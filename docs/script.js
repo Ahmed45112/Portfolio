4e4eec6256320ff50a11cf68a53d312390053571
Ahmed Mohamed <ahmedmohamed4.pro@gmail.com>
2026-09-11 08:48:48 +0200

    Nouveau portfolio : design simple bleu, pages separees (competences, projets, certificats, CV, formation, contact), certificats telechargeables

--- docs/script.js
+++
@@ -1,9 +0,0 @@
-const io = new IntersectionObserver((entries) => {
-  entries.forEach((e, i) => {
-    if (e.isIntersecting) {
-      setTimeout(() => e.target.classList.add('visible'), i * 70);
-      io.unobserve(e.target);
-    }
-  });
-}, { threshold: .1 });
-document.querySelectorAll('.reveal').forEach((el) => io.observe(el));
